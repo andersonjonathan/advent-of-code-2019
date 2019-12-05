686fa6be82d6b095856011f8d8e81690a0d98f8d
Jonathan Anderson <jonathan@jonathananderson.se>
2019-12-05 23:50:55 +0100

    Clean up of today's solution

--- day_5.py
+++ day_5.py
@@ -1,70 +1,71 @@
-# 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
-# 2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
-# 2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
-# 1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
-
-
 def computer(program):
-    def read_value(mode, pos):
+    """The computer."""
+    def read(mode, position):
         if mode == 0:
-            return program[program[int(pos)]]
+            return program[program[int(position)]]
         elif mode == 1:
-            return program[pos]
+            return program[position]
+
     counter = 0
     while counter < len(program):
-        raw_opcode = str(program[counter])
-        opcode = int(raw_opcode[-2:])
-        parameter_mode = [int(x) for x in list(raw_opcode[:-2])]
-        parameter_mode.insert(0, 0)
-        parameter_mode.insert(0, 0)
-        parameter_mode.insert(0, 0)
-        parameter_mode.insert(0, 0)
-        if opcode == 1:
-            program[program[counter + 3]] = read_value(parameter_mode[-1], counter + 1) + read_value(parameter_mode[-2], counter + 2)
+        # Explanation for the interpretation of instruction to opcode and param_mode
+        # ABCDE
+        #  1002
+        #
+        # DE - two-digit opcode,      02 == opcode 2
+        # C - mode of 1st parameter,  0 == position mode
+        # B - mode of 2nd parameter,  1 == immediate mode
+        # A - mode of 3rd parameter,  0 == position mode, omitted due to being a leading zero
+        instruction = str(program[counter])
+        opcode = int(instruction[-2:])
+        param_mode = [0 for _ in range(4)]
+        param_mode += [int(x) for x in list(instruction[:-2])]
+
+        if opcode == 1:  # Add - 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
+            program[program[counter + 3]] = read(param_mode[-1], counter + 1) + read(param_mode[-2], counter + 2)
             counter += 4
-        elif opcode == 2:
-            program[program[counter + 3]] = read_value(parameter_mode[-1], counter + 1) * read_value(parameter_mode[-2], counter + 2)
+        elif opcode == 2:  # Multiply - 2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
+            program[program[counter + 3]] = read(param_mode[-1], counter + 1) * read(param_mode[-2], counter + 2)
             counter += 4
-        elif opcode == 3:
-            program[program[counter + 1]] = int(input('input'))
+        elif opcode == 3:  # Input - 3,50 would take an input value and store it at address 50.
+            program[program[counter + 1]] = int(input('input: '))
             counter += 2
-        elif opcode == 4:
-            print(program[program[counter + 1]])
+        elif opcode == 4:  # Output - 4,50 would output the value at address 50.
+            print(read(param_mode[-1], counter + 1))
             counter += 2
-        elif opcode == 5:
-            if read_value(parameter_mode[-1], counter + 1) != 0:
-                counter = read_value(parameter_mode[-2], counter + 2)
+        elif opcode == 5:  # Jump if not 0 - 105,2,20 would change the counter to the value at address 20.
+            if read(param_mode[-1], counter + 1) != 0:
+                counter = read(param_mode[-2], counter + 2)
             else:
                 counter += 3
-        elif opcode == 6:
-            if read_value(parameter_mode[-1], counter + 1) == 0:
-                counter = read_value(parameter_mode[-2], counter + 2)
+        elif opcode == 6:  # Jump if 0 - 106,0,20 would change the counter to the value at address 20.
+            if read(param_mode[-1], counter + 1) == 0:
+                counter = read(param_mode[-2], counter + 2)
             else:
                 counter += 3
-        elif opcode == 7:
-            if read_value(parameter_mode[-1], counter + 1) < read_value(parameter_mode[-2], counter + 2):
+        elif opcode == 7:  # Less than - 1107,2,5,20 would change the value to 1 at address 20.
+            if read(param_mode[-1], counter + 1) < read(param_mode[-2], counter + 2):
                 program[program[counter + 3]] = 1
             else:
                 program[program[counter + 3]] = 0
             counter += 4
-        elif opcode == 8:
-            if read_value(parameter_mode[-1], counter + 1) == read_value(parameter_mode[-2], counter + 2):
+        elif opcode == 8:  # Equal to - 1108,2,5,20 would change the value to 0 at address 20.
+            if read(param_mode[-1], counter + 1) == read(param_mode[-2], counter + 2):
                 program[program[counter + 3]] = 1
             else:
                 program[program[counter + 3]] = 0
             counter += 4
 
-        elif opcode == 99:
+        elif opcode == 99:  # Exit
             break
         else:
-            print("kaoos")
-            break
+            raise RuntimeError('Unknown operation')
 
 
 def main():
-    computer([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
+    """Main function that loads a program to the 'computer'."""
+    computer([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125,
+              20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99])
     with open('day_5.input') as input_file:
         memory = [int(x) for x in input_file.readline().strip().split(',')]
         computer(memory)
