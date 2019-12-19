d851ffd2c6d408ec705289ee8b776a9e09dd90e2
Jonathan Anderson <jonathan@jonathananderson.se>
2019-12-19 21:08:32 +0100

    Day 16 part 1

--- day_16.py
+++ day_16.py
@@ -1,39 +1,40 @@
+import math
+from timeit import default_timer as timer
+import numpy as np
+
 def main():
     with open('day_16.input') as input_file:
         base_pattern = [0, 1, 0, -1]
-        pattern_offset = 1
-        # raw_input_data = input_file.readline().strip()
-        raw_input_data = "".join([input_file.readline().strip()] * 10000)
-        raw_input_data = "".join(['03036732577212944063491565474664'] * 10000)
+        raw_input_data = input_file.readline().strip()
+        # raw_input_data = "".join([input_file.readline().strip()] * 10000)
+        # raw_input_data = "".join(['03036732577212944063491565474664']*10000)
+        # raw_input_data = "90871223585914546619083218645595"
 
         input_data = [int(x) for x in str(raw_input_data)]
-        length = len(input_data)
         index_offset = int("".join([str(x) for x in input_data[:7]]))
-        input_data = input_data[index_offset : index_offset+8]
-        pattern_offset = index_offset
-        print(index_offset)
+        # input_data = input_data[index_offset:]
         phases = 100
+        length = len(input_data)
+        print('length', length)
+        patterns = []
+        for p in range(length):
+            pattern = []
+            for pat_val in base_pattern:
+                for _ in range(p + 1):
+                    pattern.append(pat_val)
+            pattern = pattern * (math.ceil(length / (4 * (p+1))) + 1)
+            patterns.append(pattern[1:length + 1])
+        a = np.array(patterns)
+
+        start = timer()
         for phase in range(phases):
-            print(phase)
-            result = []
-            pattern_offset = 1
-            for p in range(length)[index_offset : index_offset+8]:
-                pattern = []
-                for pat_val in base_pattern:
-                    for _ in range(p + 1):
-                        pattern.append(pat_val)
-                iteration_result = []
-                for i in range(len(input_data)):
-                    iteration_result.append(pattern[(i+pattern_offset) % len(pattern)] * input_data[i])
-
-                result.append(int(str(sum(iteration_result))[-1]))
-            input_data = result.copy()
+            b = np.array(input_data)
+            input_data = [int(str(x)[-1]) for x in a.dot(b)]
+        end = timer()
+        print(end - start)
         # print("".join([str(x) for x in result]))
-        data = "".join([str(x) for x in result])
+        data = "".join([str(x) for x in input_data])
         print(data)
-
-
-
 
 
 if __name__ == '__main__':
